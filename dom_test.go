137e2bf1898bd739595fd984d39a83b0d4f886ab
Marc Lavergne <mlavergn@Marcs-MacBook-Pro.local>
2016-09-11 10:15:57 -0400

    Added mechanism for loading test data

--- dom_test.go
+++ dom_test.go
@@ -6,6 +6,10 @@ package godom
 
 import (
   "testing"
+  "runtime"
+  "path"
+  "io/ioutil"
+  "log"
 )
 
 func TestDOMSetContents(t *testing.T) {
@@ -14,4 +18,31 @@ func TestDOMSetContents(t *testing.T) {
   if x.ContentSize() != 13 {
     t.Errorf("ContentSize %d vs expected %d", x.ContentSize(), 13)
   }
+}
+
+func _loadData(t *testing.T, fileName string) (contents string) {
+  _, filename, _, _ := runtime.Caller(0)
+  contentPath := path.Join(path.Dir(filename), "_testdata", "test_a.html")
+  bytes, err := ioutil.ReadFile(contentPath)
+  if err != nil {
+    t.Errorf("%s", err)
+  }
+
+  contents = string(bytes)
+
+  return contents
+}
+
+func TestFind(t *testing.T) {
+  contents := _loadData(t, "test_a.html")
+
+  d := NewDOM()
+  d.SetContents(contents)
+  
+  meta := d.Find("meta", nil)
+  if meta == nil {
+    t.Errorf("failed to find META")
+  }
+
+  log.Println(meta)
 }
