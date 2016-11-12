f6b6c49932313d4642786f1af77c05565cb6310f
Marc Lavergne <mlavergn@gmail.com>
2016-11-12 01:53:39 -0500

    Add support for descendant set lookups

--- dom.go
+++ dom.go
@@ -5,58 +5,60 @@
 package godom
 
 import (
+	."golog"
+	"golang.org/x/net/html"
 	"encoding/json"
 	"fmt"
-	"golang.org/x/net/html"
 	"strings"
+	"strconv"
 )
 
-// NodeAtributes map of strings keyed by strings
-type NodeAttributes map[string]string
+// DOMNodeAttributes map of strings keyed by strings
+type DOMNodeAttributes map[string]string
 
 // JSONMap map of interface keyed by strings
 type JSONMap map[string]interface{}
 
-//
-// DOM Node
+// DOMNode def
 //
 type DOMNode struct {
+	Index      int
 	Tag        string
-	Attributes NodeAttributes
+	Attributes DOMNodeAttributes
 	Text       string
 	Parent     *DOMNode
 	Children   []*DOMNode
 }
 
 //
-// Node: Constructor.
+// NewDOMNode constructor
 //
-func NewDOMNode(parent *DOMNode, tag string, attributes NodeAttributes) *DOMNode {
+func NewDOMNode(parent *DOMNode, tag string, attributes DOMNodeAttributes) *DOMNode {
 	return &DOMNode{Parent: parent, Children: []*DOMNode{}, Tag: strings.ToLower(tag), Attributes: attributes}
 }
 
 //
 // Node: String representation.
 //
-func (self *DOMNode) String() (desc string) {
+func (id *DOMNode) String() (desc string) {
 	desc = ""
-	if self.Parent != nil {
-		desc += "Parent: " + self.Parent.Tag + "\n"
+	desc += fmt.Sprintf("\nIndex:\t%d\nTag:\t%s\nAttr:\t%s\nText:\t%s\n", id.Index, id.Tag, id.Attributes, id.Text)
+	if id.Parent != nil {
+		desc += "Parent:\t" + strconv.Itoa(id.Parent.Index) + "-" + id.Parent.Tag + "\n"
 	}
-	desc += fmt.Sprintf("Tag:\t%s\nAttr:\t%s\nText:\t%s\n", self.Tag, self.Attributes, self.Text)
-	if len(self.Children) != 0 {
-		for _, child := range self.Children {
-			desc += "Child: " + child.Tag + "\n"
+	if len(id.Children) != 0 {
+		for _, child := range id.Children {
+			desc += "Child:\t" + child.Tag + "\n"
 		}
 	}
 	return desc
 }
 
 //
-// Node: String with value of the provided attribute key.
+// Attr Node: String with value of the provided attribute key.
 //
-func (self *DOMNode) Attr(key string) string {
-	return self.Attributes[key]
+func (id *DOMNode) Attr(key string) string {
+	return id.Attributes[key]
 }
 
 //
@@ -69,52 +71,83 @@ type DOM struct {
 }
 
 //
-// DOM: Constructor.
+// NewDOM Constructor
 //
 func NewDOM() *DOM {
-	self := &DOM{}
-	self.nodes = make(map[string][]*DOMNode)
-	return self
+	id := &DOM{}
+	id.nodes = make(map[string][]*DOMNode)
+	return id
 }
 
 //
 // DOM: String representation.
 //
-func (self *DOM) String() (result string) {
+func (id *DOM) String() (result string) {
 	result = ""
-	for _, node := range self.document {
+	for _, node := range id.document {
 		result += fmt.Sprintf("Node:\n%s\n", node)
 	}
 	return result
 }
 
 //
-// DOM: Parse the raw html contents.
+// SetContents : parse the raw html contents.
 //
-func (self *DOM) SetContents(html string) {
-	self.contents = html
-	self._parse(html)
+func (id *DOM) SetContents(html string) {
+	id.contents = html
+	id._parse(html)
 }
 
 //
-// DOM: The raw html contents.
+// Contents : The raw html contents.
 //
-func (self *DOM) Contents() string {
-	return self.contents
+func (id *DOM) Contents() string {
+	return id.contents
 }
 
 //
-// DOM: The byte count of the raw html contents.
+// ContentLength : The byte count of the raw html contents.
 //
-func (self *DOM) ContentSize() int {
-	return len(self.Contents())
+func (id *DOM) ContentLength() int {
+	return len(id.Contents())
+}
+
+//
+// RootNode : The HTML root node
+//
+var rootNode *DOMNode
+func (id *DOM) RootNode() (result *DOMNode) {
+	if rootNode == nil {
+		// we're looking for the tidy-ed HTML node at index 1
+		// there's the childless DOCUMENT node at index 0
+		for i:=0; i<len(id.document); i++ {
+			if id.document[i].Tag == "html" {
+				rootNode = id.document[i]
+			}
+		}
+	}
+
+	return rootNode
+}
+
+//
+// DumpNodes : The byte count of the raw html contents.
+//
+func (id *DOM) DumpNodes() {
+		LogInfo(id.document)
+}
+
+func (id *DOM) DumpRelationships() {
+	for _, node := range id.document {
+		LogInfo(node.Parent)
+	}
 }
 
 //
 // DOM: Parse the Token attributes into a map.
 //
-func (self *DOM) _nodeAttributes(node *html.Node) (attrs NodeAttributes) {
-	attrs = make(NodeAttributes)
+func (id *DOM) _nodeAttributes(node *html.Node) (attrs DOMNodeAttributes) {
+	attrs = make(DOMNodeAttributes)
 
 	for _, attr := range node.Attr {
 		attrs[attr.Key] = attr.Val
@@ -126,11 +159,11 @@ func (self *DOM) _nodeAttributes(node *html.Node) (attrs NodeAttributes) {
 //
 // DOM: Parse the Token attributes into a map.
 //
-func (self *DOM) _parseFragment(parent *DOMNode, root *html.Node, contents string) {
+func (id *DOM) _parseFragment(parent *DOMNode, root *html.Node, contents string) {
 	nodes, err := html.ParseFragment(strings.NewReader(contents), root)
 	if err == nil {
 		for _, node := range nodes {
-			self._walk(parent, node, true)
+			id._walk(parent, node, true)
 		}
 	}
 }
@@ -138,36 +171,37 @@ func (self *DOM) _parseFragment(parent *DOMNode, root *html.Node, contents strin
 //
 // DOM: Walk the DOM and parse the HTML tokens into Nodes.
 //
-func (self *DOM) _walk(parent *DOMNode, root *html.Node, fragment bool) {
+func (id *DOM) _walk(parent *DOMNode, root *html.Node, fragment bool) {
 	parseSkipTags := map[string]int{"script": 1, "style": 1, "body": 1}
 	fragmentSkipTags := map[string]int{"html": 1, "head": 1, "body": 1}
 
 	switch root.Type {
 	case html.ElementNode:
 		if !fragment || (fragment && fragmentSkipTags[root.Data] == 0) {
-			domNode := NewDOMNode(parent, root.Data, self._nodeAttributes(root))
+			domNode := NewDOMNode(parent, root.Data, id._nodeAttributes(root))
 			// set the children and swap
 			if parent != nil {
 				parent.Children = append(parent.Children, domNode)
 			}
 			parent = domNode
-			self.document = append(self.document, domNode)
-			nodeArr := self.nodes[domNode.Tag]
+			id.document = append(id.document, domNode)
+			domNode.Index = len(id.document)
+			nodeArr := id.nodes[domNode.Tag]
 			if nodeArr != nil {
-				self.nodes[domNode.Tag] = append(nodeArr, domNode)
+				id.nodes[domNode.Tag] = append(nodeArr, domNode)
 			} else {
-				self.nodes[domNode.Tag] = []*DOMNode{domNode}
+				id.nodes[domNode.Tag] = []*DOMNode{domNode}
 			}
 		}
 	case html.TextNode:
 		text := strings.TrimSpace(root.Data)
 		if len(text) > 0 {
 			if root.Parent == nil || parseSkipTags[root.Parent.Data] == 0 {
-				self._parseFragment(parent, root.Parent, text)
+				id._parseFragment(parent, root.Parent, text)
 			} else {
-				dlen := len(self.document)
+				dlen := len(id.document)
 				if dlen > 0 {
-					owningNode := self.document[dlen-1]
+					owningNode := id.document[dlen-1]
 					if owningNode != nil {
 						owningNode.Text = strings.TrimSpace(root.Data)
 					}
@@ -175,41 +209,45 @@ func (self *DOM) _walk(parent *DOMNode, root *html.Node, fragment bool) {
 			}
 		}
 	case html.CommentNode:
-		domNode := NewDOMNode(parent, "comment", self._nodeAttributes(root))
-		self.document = append(self.document, domNode)
+		domNode := NewDOMNode(parent, "comment", id._nodeAttributes(root))
+		id.document = append(id.document, domNode)
+		domNode.Index = len(id.document)
 	case html.ErrorNode:
-		domNode := NewDOMNode(parent, "error", self._nodeAttributes(root))
-		self.document = append(self.document, domNode)
+		domNode := NewDOMNode(parent, "error", id._nodeAttributes(root))
+		id.document = append(id.document, domNode)
+		domNode.Index = len(id.document)
 	case html.DocumentNode:
-		domNode := NewDOMNode(parent, "document", self._nodeAttributes(root))
-		self.document = append(self.document, domNode)
+		domNode := NewDOMNode(parent, "document", id._nodeAttributes(root))
+		id.document = append(id.document, domNode)
+		domNode.Index = len(id.document)
 	case html.DoctypeNode:
-		domNode := NewDOMNode(parent, "doctype", self._nodeAttributes(root))
-		self.document = append(self.document, domNode)
+		domNode := NewDOMNode(parent, "doctype", id._nodeAttributes(root))
+		id.document = append(id.document, domNode)
+		domNode.Index = len(id.document)
 	}
 
 	for child := root.FirstChild; child != nil; child = child.NextSibling {
-		self._walk(parent, child, fragment)
+		id._walk(parent, child, fragment)
 	}
 }
 
 //
 // DOM: Walk the DOM and parse the HTML tokens into Nodes.
 //
-func (self *DOM) _parse(contents string) {
+func (id *DOM) _parse(contents string) {
 	doc, err := html.Parse(strings.NewReader(contents))
 	if err == nil {
-		self._walk(nil, doc, false)
+		id._walk(nil, doc, false)
 	}
 }
 
 //
+// DumpLinks : Show the a hrefs in the DOM
 //
-//
-func (self *DOM) DumpLinks() (result []string) {
+func (id *DOM) DumpLinks() (result []string) {
 	result = make([]string, 100)
 
-	tagNodes := self.nodes["a"]
+	tagNodes := id.nodes["a"]
 	for _, node := range tagNodes {
 		attr := node.Attributes
 		value := attr["href"]
@@ -222,11 +260,62 @@ func (self *DOM) DumpLinks() (result []string) {
 }
 
 //
-// DOM: Find the Node of type tag with the specified attributes
+// IsDescendantNode : Is node a descendant of parent?
+// The fastest confirmation is bottom up since the relationships are
+// one to many.
 //
-func (self *DOM) Find(tag string, attributes NodeAttributes) (result []*DOMNode) {
+func (id *DOM) IsDescendantNode(parent *DOMNode, node *DOMNode) (result bool) {
+	result = false
+	// nil and equality check
+	if parent == nil {
+		result = true
+	} else if node == nil {
+		result = false
+	} else if parent == node {
+		result = true
+	} else {
+		// we would have matched above if parent and node were the root node
+		root := id.RootNode()
+		for node != nil && parent.Index <= node.Index && node.Index != root.Index {
+			if node.Parent.Index == parent.Index {
+				result = true
+				break
+			} else {
+				node = node.Parent
+			}
+		}
+	}
+
+	return result
+}
+
+//
+// IsChildNode : Is node a child of parent?
+// The fastest confirmation is bottom up since the relationships are
+// one to many.
+//
+func (id *DOM) IsChildNode(parent *DOMNode, node *DOMNode) (result bool) {
+	result = false
+	if node != nil && node.Parent == parent {
+		result = true
+	}
+
+	return result
+}
+
+//
+// Find : Find the Node of type tag with the specified attributes
+//
+func (id *DOM) Find(tag string, attributes DOMNodeAttributes) (result []*DOMNode) {
+	return id.NodeFind(id.RootNode(), tag, attributes)
+}
+
+//
+// NodeFind : Find the child Node of type tag with the specified attributes
+//
+func (id *DOM) NodeFind(parent *DOMNode, tag string, attributes DOMNodeAttributes) (result []*DOMNode) {
 	found := true
-	tagNodes := self.nodes[tag]
+	tagNodes := id.nodes[tag]
 	for _, node := range tagNodes {
 		// found a matching tag
 		nodeAttrs := node.Attributes
@@ -237,7 +326,9 @@ func (self *DOM) Find(tag string, attributes NodeAttributes) (result []*DOMNode)
 			}
 		}
 		if found {
-			result = append(result, node)
+			if id.IsDescendantNode(parent, node) {
+				result = append(result, node)
+			}
 		}
 	}
 
@@ -245,16 +336,25 @@ func (self *DOM) Find(tag string, attributes NodeAttributes) (result []*DOMNode)
 }
 
 //
-// DOM: Find the Node of type tag with text containing key
+// FindWithKey : Find the Node of type tag with text containing key
 //
-func (self *DOM) FindWithKey(tag string, substring string) (result []*DOMNode) {
-	tagNodes := self.nodes[tag]
+func (id *DOM) FindWithKey(tag string, substring string) (result []*DOMNode) {
+	return id.NodeFindWithKey(id.RootNode(), tag, substring)
+}
+
+//
+// NodeFindWithKey : Find the child Node of type tag with text containing key
+//
+func (id *DOM) NodeFindWithKey(parent *DOMNode, tag string, substring string) (result []*DOMNode) {
+	tagNodes := id.nodes[tag]
 	for _, node := range tagNodes {
 		// found a matching tag
-		contents := node.Text
-		idx := strings.Index(contents, substring)
-		if idx >= 0 {
-			result = append(result, node)
+		if id.IsDescendantNode(parent, node) {
+			contents := node.Text
+			idx := strings.Index(contents, substring)
+			if idx >= 0 {
+				result = append(result, node)
+			}
 		}
 	}
 
@@ -262,10 +362,17 @@ func (self *DOM) FindWithKey(tag string, substring string) (result []*DOMNode) {
 }
 
 //
-// DOM: Find the given tag with the specified attributes
+// FindTextForClass : Find the given tag with the specified attributes
 //
-func (self *DOM) FindTextForClass(tag string, class string) (result string) {
-	nodes := self.Find(tag, NodeAttributes{"class": class})
+func (id *DOM) FindTextForClass(tag string, class string) (result string) {
+	return id.NodeFindTextForClass(id.RootNode(), tag, class)
+}
+
+//
+// NodeFindTextForClass : Find the child given tag with the specified attributes
+//
+func (id *DOM) NodeFindTextForClass(parent *DOMNode, tag string, class string) (result string) {
+	nodes := id.NodeFind(parent, tag, DOMNodeAttributes{"class": class})
 
 	if len(nodes) > 0 {
 		result = nodes[0].Text
@@ -275,10 +382,17 @@ func (self *DOM) FindTextForClass(tag string, class string) (result string) {
 }
 
 //
-// DOM: Find the JSON key with text containing substring
+// FindJSONForScriptWithKey : Find the JSON key with text containing substring
 //
-func (self *DOM) FindJsonForScriptWithKey(substring string) (result JSONMap) {
-	nodes := self.FindWithKey("script", substring)
+func (id *DOM) FindJSONForScriptWithKey(substring string) (result JSONMap) {
+	return id.NodeFindJSONForScriptWithKey(id.RootNode(), substring)
+}
+
+//
+// NodeFindJSONForScriptWithKey : Find the child JSON key with text containing substring
+//
+func (id *DOM) NodeFindJSONForScriptWithKey(parent *DOMNode, substring string) (result JSONMap) {
+	nodes := id.NodeFindWithKey(parent, "script", substring)
 
 	if len(nodes) > 0 {
 		contents := nodes[0].Text
